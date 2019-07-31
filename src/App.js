6bb983b4d7866b08599d40eef2edaf42e0eb4179
sunukim <sunukim@afreecatv.com>
2019-07-31 17:37:00 +0900

    #2 끝

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F"
+  },
+  {
+    title: "Oldboy",
+    poster: "http://movie.phinf.naver.net/20111222_177/1324537084439rmrVk_JPEG/movie_image.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://vignette.wikia.nocookie.net/starwars/images/f/fd/Star_Wars_Episode_VII_The_Force_Awakens.jpg/revision/latest?cb=20160229135720&path-prefix=ko"
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.map((movie, index) => {
+        return <Movie title={movie.title} poster={movie.poster} key={index}/>
+      })}
     </div>
   );
 }
